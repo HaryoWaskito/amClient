495f1fdd2e96f018803fdff0c18956e3de7fa8e8
Haryo Waskito <haryo.way@gmail.com>
2017-08-03 20:23:41 +0700

    Backup First

---
+++ amClient/ModelScreenCapture.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace amClient
+{
+    public class ModelScreenCapture
+    {
+        public string ModelScreenCaptureId { get; set; }
+
+        public int SessionID { get; set; }
+
+        public string ActivityName { get; set; }
+
+        public byte[] Image { get; set; }
+
+        public DateTime CaptureScreenDate { get; set; }
+
+        public bool IsSuccessSendToServer { get; set; }
+    }
+}
